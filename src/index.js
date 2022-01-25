83d08699d4358b93444f13fe8afda3d716dba8ce
od-c0d3r <omar_rashad@aol.com>
2022-01-25 11:02:53 +0200

    Repo: remove redundant file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 
