f0b1124a05d11a36b0cc60c2f807927fd453d26e
od-c0d3r <omar_rashad@aol.com>
2022-01-25 10:58:54 +0200

    Add: missing Provider component for redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
