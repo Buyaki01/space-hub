e126c321dbaac3d06291b0b1009dd713a91b2538
od-c0d3r <omar_rashad@aol.com>
2022-01-25 10:26:59 +0200

    Setup: add react-bootstrap to index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 
 ReactDOM.render(
