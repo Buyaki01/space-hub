534c2323610af7fdaa4d1c16367b4e6d10fcc1f7
od-c0d3r <omar_rashad@aol.com>
2022-01-27 08:12:45 +0200

    Handle: cancel action for dragons feature

--- src/components/Pages/DragonsPage.js
+++ src/components/Pages/DragonsPage.js
@@ -5,16 +5,24 @@ import {
   Button,
 } from 'react-bootstrap';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleDragonReservation } from '../../redux/dragons/dragonReducer';
 
 const DragonsPage = () => {
+  const dispatch = useDispatch();
   const dragons = useSelector(({ dragonsReducer }) => dragonsReducer.dragons);
+
+  const handleClick = (id) => {
+    console.log(id);
+    dispatch(toggleDragonReservation(id));
+  };
+
   const dragonsList = dragons.map((dragon) => {
     const readMoreLink = <a href={dragon.wikipedia} target="_blank" rel="noreferrer noopener">Read More</a>;
     return (
       <Col key={dragon.id}>
-        <Card style={{ height: '520px' }}>
-          <Card.Img variant="top" src={dragon.flickr_images[0]} />
+        <Card style={{ height: '550px' }}>
+          <Card.Img variant="top" src={dragon.flickr_images[0]} style={{ height: '300px' }} />
           <Card.Body className="d-flex flex-column justify-content-between">
             <div>
               <Card.Title>{dragon.name}</Card.Title>
@@ -23,7 +31,8 @@ const DragonsPage = () => {
                 {readMoreLink}
               </Card.Text>
             </div>
-            <Button variant="primary">Go somewhere</Button>
+            {!dragon.reserved && (<Button variant="primary" onClick={() => handleClick(dragon.id)}>Reserve</Button>)}
+            {dragon.reserved && (<Button variant="outline-danger" onClick={() => handleClick(dragon.id)}>Cancel</Button>)}
           </Card.Body>
         </Card>
       </Col>
--- src/redux/dragons/dragonReducer.js
+++ src/redux/dragons/dragonReducer.js
@@ -1,4 +1,5 @@
 export const INIT_DRAGONS = 'spaceHub/dragons/INIT_DRAGONS';
+export const TOGGLE_DRAGON_RESERVATION = 'spaceHub/dragons/TOGGLE_DRAGON';
 
 const iniState = { dragons: [] };
 
@@ -7,11 +8,24 @@ export const initDragons = (payload) => ({
   payload,
 });
 
+export const toggleDragonReservation = (payload) => ({
+  type: TOGGLE_DRAGON_RESERVATION,
+  payload,
+});
+
 const reducer = (state = iniState, action) => {
+  const newDragonsAfterToggle = state.dragons.map(
+    (dragon) => (dragon.id !== action.payload ? dragon : { ...dragon, reserved: !dragon.reserved }),
+  );
+
   switch (action.type) {
     case INIT_DRAGONS:
       return {
         dragons: action.payload,
+      };
+    case TOGGLE_DRAGON_RESERVATION:
+      return {
+        dragons: newDragonsAfterToggle,
       };
     default:
       return state;
