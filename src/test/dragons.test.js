43ea7791f14eddec98867c2632fe1f0446c1f8ca
od-c0d3r <omar_rashad@aol.com>
2022-01-27 13:12:24 +0200

    Test: write tests for DragonsPage component

---
+++ src/test/dragons.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import '@testing-library/jest-dom'
+import store from '../redux/store';
+
+import DragonsPage from '../components/Pages/DragonsPage';
+
+jest.mock('../modules/dragonsAPI');
+
+describe('<DragonsPage />', () => {
+    test('Render a text when no dragons data available', () => {
+        render(
+            <Provider store={store}>
+                <DragonsPage />
+            </Provider>
+        );
+        const text = screen.getByText(/No Data Available/);
+        expect(text).toBeInTheDocument();
+    });
+
+    test('Test DragonPage Render', () => {
+        const tree = render(
+            <Provider store={store}>
+                <DragonsPage />
+            </Provider>,
+        );
+        expect(tree).toMatchSnapshot();
+    });
+});
